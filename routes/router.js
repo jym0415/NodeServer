c4bf565af1006b8c95ec9f2e1c49a05f8be98277
jym0415 <jym9504@gmail.com>
2022-02-28 17:45:49 +0900

    proxy

--- routes/router.js
+++ routes/router.js
@@ -5,8 +5,8 @@ var router = express.Router(); //가져오기
 router.get('/',function(req, res){//요청,응답
     res.send('/user 나와랏') //send 는 글자만 보낼수있음
 })
-router.get('/company',function(req, res){//요청,응답
-    res.send('/user/company 화면임') //send 는 글자만 보낼수있음
+router.get('/22',function(req, res){//요청,응답
+    res.send('/22222') //send 는 글자만 보낼수있음
 })
 
 
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
-// var projectlink = require('./routes/router');
+var aaa = require('./routes/router');
 
-// app.get('/',function(req, res){//요청,응답
-//     res.send('나와랏') //send 는 글자만 보낼수있음
-// })
-// app.use('/user',projectlink);
+app.get('/',function(req, res){//요청,응답
+    res.send('나와랏') //send 는 글자만 보낼수있음
+})
+app.use('/user',aaa);
+
 
 app.get('/me',function(req, res){
     res.send('나의 소개')
@@ -23,4 +24,5 @@ app.get('/contact',function(req, res){
 app.listen( 5000,function(){
     console.log('서버구동완료!~~!!');
 })
+
 
